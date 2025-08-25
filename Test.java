88f38b39c5b132002d505814ba9913fb302d345f
Himanshu tiwari <68557180+HIMANSHUTIWARI14071999@users.noreply.github.com>
2025-08-25 14:19:55 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test {
+  public static void main(String[] args) {
+    system.out.println("------------------------------------------------------------------------------------------")
+    system.out.println("Helo from Github")
+    system.out.println("------------------------------------------------------------------------------------------")
+      }
+}
